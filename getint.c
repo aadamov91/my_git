fabd2df2e7f8c4abd1fb1eac5008b1953fe94af1
aadamov91 <alexadamov91@gmail.com>
2016-03-30 13:39:30 -0400

    getint test

---
+++ getint.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 100
+#define BUFS 100
+
+main(void)
+{
+	int n, i;
+	int array[MAX];
+	int getint (int *);
+
+	for (n = 0; n < MAX && getint(&array[n]) != EOF; n++)
+		printf ("array%d is %d\n", n,  array[n]);
+
+	for (i = 0; i < n; i++)
+		printf ("%d", array[i]);
+	printf("\nend\n");
+}
+
+int getch(void);
+void ungetch (int );
+
+char buf[BUFS];
+int bufp = 0;
+
+
+int getint(int *pn)
+{
+	int c, sign;
+	
+	printf("\nbuffp is %d\n", bufp );
+	
+	while (isspace(c = getchar()))
+	;
+
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-'){
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+		c = getch();
+	for (*pn = 0; isdigit(c); c = getch())
+		*pn = *pn + (c - '0');
+	*pn *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
+
+int getch(void)
+{
+	
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFS)
+		printf("ungetch error buf is over\n");
+	else 
+		buf[bufp++] = c;
+}
